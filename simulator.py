217bfdda3a7ad5cc448ba7a73010eb07770e0780
Pedro Boada <pboada@openlab.pe>
2020-10-21 11:01:26 -0500

    Update simulator.py
    Update parameters of the objects

--- simulator.py
+++ simulator.py
@@ -5,17 +5,26 @@ from functions import *
 Varioplus = Plant("Varioplus")
 
 # Create all the equipments inside the Plant (Machines and Stations)
-# (Name, Min_speed, Max_speed, Operators, Distance_to_previous, Distance_to_next, Specific_handling, Place)
-Cnc_laser       = Equipment("CNC Laser Machine",          590.0000,   610.0000, 1, 1.45, 2.0, 0.28, Varioplus)
+# Used units: meters (m), minutes (min)
+# (Name, Min_speed, Max_speed, Operators, Distance_to_previous, Distance_to_next, Specific_handling_in, Specific_handling_out, Plant_to place)
+Cnc_laser = Equipment("CNC Laser Machine", 170.00, 190.00, 1, 2.0, 2.0, 1.0, 1.0, Varioplus)
 
 # Create all the Processes inside the Plant
 # (Name, Kind_of_process, Increase_pcs?, Need_mod, Equipment, Place)
-Laser_cutting   = Process("Laser cutting",          "Perimeter",    True,   True,   Cnc_laser,      Varioplus)
+
+# Name: the name of the process.
+# Kind_of_process: how the process is applied? by lenght, by area, by unit.
+# Increase_pcs?: does this process increase pieces? True or False.
+# Need_mod: does this procees need modulation? Trued or False.
+# Equipment: which equipment are we working on?
+# Place: where are we working?
+
+Laser_cutting   = Process("Laser cutting", "Lenght", True, True, Cnc_laser, Varioplus)
 
 # Create all the Models inside the Plant
-# (Name, Perimeter, Area, Kc_long, Pcs_mod, Mod_time, Hand_i, Hand_o, Place)
+# (Name, Perimeter, Area, Kc_long, Wire_long, Pcs_mod, Hand_i, Hand_o, Place)
 
-Model_1         = Model("Name", 430.40, 11615.59, 320.10, 1.00, 2.15, 1.00, 1.00, Varioplus)
+Model_1 = Model("Name", 430.40, 11615.59, 320.10, 1.00, 1.00, 1.00, Varioplus)
 
 # Scheduling and running
 
